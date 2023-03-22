c0309484190a82f51b394df6b9a6af5163fca995
alperarinc <alperarinc@hotmail.com>
2023-03-22 23:59:29 +0300

    WebSockets

---
+++ src/test/java/co/tr/redoxkali/co/tr/redoxkali/WebsocketsTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package co.tr.redoxkali.co.tr.redoxkali;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsocketsTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
